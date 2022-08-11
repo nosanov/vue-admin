187dda2eb7abcf5441c41ab88864994321d90269
Sergey Nosanov <yegresvonason@gmail.com>
2022-08-12 00:28:37 +0300

    Добавил нумерацию к списку

--- app/api/deleteHtmlPage.php
+++ app/api/deleteHtmlPage.php
@@ -1,5 +1,8 @@
 <?php
-$deleteFile = "../../" . $_POST["name"] . ".html";
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$deleteFile = "../../" . $_POST["name"];
 
 if (!file_exists($deleteFile)) {
   header("HTTP/1.0 400 Bad Request");
--- app/src/main.js
+++ app/src/main.js
@@ -1,36 +1,36 @@
-const $ = require("jquery");
+const Vue = require("vue/dist/vue");
+const axios = require("axios");
 
-function getPagesList() {
-  $('h1').remove();
-  $.get("./api/api.php", (data) => {
-    data.forEach((file) => {
-      $("body").append("<h1>" + file + "</h1>");
-    })
-  }, "JSON");
-}
-
-getPagesList();
-
-// create page
-$('.create-page_button').click(() => {
-  $.post('./api/createNewHtmlPage.php', {
-    "name": $('.create-page_input').val()
-  }, () => {
-    getPagesList();
-  })
-  .fail(() => {
-    alert('Такая страница уже существует!');
-  });
+new Vue({
+    el: "#app",
+    data: {
+        pageList: [],
+        newPageName: ""
+    },
+    methods: {
+        createPage() {
+            axios
+                .post("./api/createNewHtmlPage.php", {
+                    "name": this.newPageName
+                })
+                .then(() => this.updatePageList())
+        },
+        updatePageList() {
+            axios
+                .get("./api/api.php")
+                .then((response) => {
+                    this.pageList = response.data
+                })
+        },
+        deletePage(page) {
+            axios
+                .post("./api/deleteHtmlPage.php", {
+                    "name": page
+                })
+                .then(() => this.updatePageList())
+        }
+    },
+    created() {
+        this.updatePageList();
+    },
 });
-
-// delete page
-$('.delete-page_button').click(() => {
-  $.post('./api/deleteHtmlPage.php', {
-    "name": $('.delete-page_input').val()
-  }, () => {
-    getPagesList();
-  })
-  .fail(() => {
-    alert('Такой страницы не существует!');
-  });
-})
